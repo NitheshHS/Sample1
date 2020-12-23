25989d4b67989f25b381d114977352891f85b149
NitheshHS <nitheshsgowda4.ng@gmail.com>
2020-12-23 11:38:21 +0530

    Maths class is added

---
+++ src/test/java/sample/Maths.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class Maths {
+public static void main(String[] args) {
+	int a=10;
+	int b=20;
+	System.out.println(sum(a, b));
+}
+public static int sum(int a,int b) {
+	return a+b;
+}
+}
