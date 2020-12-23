e3b847e755c7e712a6e000c4da1ebc27ae88eecd
NitheshHS <nitheshsgowda4.ng@gmail.com>
2020-12-23 11:48:50 +0530

    Math class modified

--- src/test/java/sample/Maths.java
+++ src/test/java/sample/Maths.java
@@ -5,8 +5,12 @@ public static void main(String[] args) {
 	int a=10;
 	int b=20;
 	System.out.println(sum(a, b));
+	System.out.println(multiply(a, b));
 }
 public static int sum(int a,int b) {
 	return a+b;
+}
+public static int multiply(int a,int b) {
+	return a*b;
 }
 }
